fbbfee8ae4052fd9e7263e3a66a151c0e20fa181
pebriniel <zeldalyoko@gmail.com>
2017-03-04 11:48:13 +0100

    update with init.php

---
+++ include/.init.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ADRESS', 'localhost');
+define('LOGIN', 'root');
+define('MDP', 'password');
+define('DB', 'loisirpourtous');
+
+define('URL_SITE', "http://".$_SERVER['HTTP_HOST']."")
